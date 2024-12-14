3f96ee99117f9bdc9be20caea3f1bbea798c1684
CarlosEduJs <carloseduardogit@gmail.com>
2024-12-14 15:52:28 -0300

    re

--- src/context/CardsContext.jsx
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext, useState } from "react";
-
-const CardsContext = createContext()
-
-export const CardProvider  = ({chidren}) => {
-    const [type, setType] = useState("annually")
-    
-}
